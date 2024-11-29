023082bfd93ec328429e870bd5171f93acf50b99
Nelvazz <waksoo666@gmail.com>
2024-11-30 00:15:41 +0100

    Version 1.0.0

--- bin/cli.js
+++ bin/cli.js
@@ -1,27 +1,72 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
-const path = require("path");
+const FS = require("fs");
+const PATH = require("path");
 require("colors");
 const { lint } = require("../lib/linter");
-const rules = require("../lib/rules");
-const config = require(path.resolve("./linter.config.json"));
+const RULES = require("../lib/rules");
+const CONFIG = require(PATH.resolve("./linter.config.json"));
 
-const [,, filePath] = process.argv;
+const [,, PARAMETER] = process.argv;
 
-if (!filePath) {
-  console.error("Error: No file specified.".red);
-  process.exit(1);
+if (!PARAMETER) {
+    console.error("Error: No file specified.".red);
+    process.exit(1);
 }
 
-const code = fs.readFileSync(path.resolve(filePath), "utf8");
-const results = lint(code, rules, config);
+switch (PARAMETER) {
+    case '-all':
+    case '-a':
+        const FILES = getAllFiles(process.cwd());
+        FILES.forEach(lintFile);
+        break;
+    default:
+        const TARGET = PATH.resolve(PARAMETER);
+        if (!FS.existsSync(TARGET)) {
+            console.error("Error: This file or folder does not exist.".red);
+            process.exit(1);
+        }
+        const STAT = FS.statSync(TARGET);
 
-if (results.length === 0) {
-  console.log("✅ No issues found!".green);
-} else {
-  console.log("❌ Issues detected:".red);
-  results.forEach((issue, idx) =>
-    console.log(`${idx + 1}. ${issue.message}`.yellow)
-  );
+        if (STAT.isDirectory()) {
+            const FILES = getAllFiles(TARGET);
+            FILES.forEach(lintFile);
+        } else {
+            lintFile(TARGET);
+        }
+        break;
+}
+
+function getAllFiles(folderPath) {
+    const FILES = [];
+    const ITEMS = FS.readdirSync(folderPath);
+
+    for (const ITEM of ITEMS) {
+        const FULLPATH = PATH.join(folderPath, ITEM);
+        const STAT = FS.statSync(FULLPATH);
+
+        if (STAT.isDirectory() && !CONFIG.ignore.some(word => FULLPATH.includes(word))) {
+            FILES.push(...getAllFiles(FULLPATH));
+        } else if (ITEM.endsWith(".js")) {
+            FILES.push(FULLPATH);
+        }
+    }
+    return FILES;
+}
+
+function lintFile(file) {
+    const CODE = FS.readFileSync(PATH.resolve(file), "utf8");
+    const RESULTS = lint({
+        name: PATH.parse(file).base,
+        content: CODE
+    }, RULES, CONFIG);
+
+    if (RESULTS.length === 0) {
+        console.log(`-`.gray+` ${file.replace(process.cwd(), '')} : ✅ No issues found!`.green);
+    } else {
+        console.log(`┌`.gray+` ${file.replace(process.cwd(), '')} : ❌ Issues detected`.red);
+        RESULTS.forEach((issue, idx) =>
+            console.log(`${RESULTS.length !== idx + 1 ? "├" : "└"}`.gray+` ${idx + 1}. ${issue.message}`.yellow)
+        );
+    }
 }
